3756353f4e0b9493871fd773cd82eeee311611a7
Sanjeev Kumar <sanjeev.siderfighter@gmail.com>
2023-11-24 18:45:38 +0530

    :tada: Create a new project

---
+++ app/src/main/java/com/example/phonepeproject/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.phonepeproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
